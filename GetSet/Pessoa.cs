04f60e396ea130acb707bdc8e44238b269bb3f47
Gustavo <g.ferreira.moreira2@gmail.com>
2024-03-28 19:05:27 -0300

    Aula de Get e Set

---
+++ GetSet/Pessoa.cs
@@ -0,0 +1,13 @@
+class Pessoa
+{
+
+    private string? nome;
+
+    public string Nome
+    {
+        get{return nome;}
+
+        set{nome=value;}
+    }
+
+}
---
+++ GetSet/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa();
+        p.Nome = "Gustavo";
+        
+        Console.WriteLine(p.Nome);
+    }
+}
